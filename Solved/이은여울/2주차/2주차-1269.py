d0d118164f0cc301f805780b87e0780f8c262572
Eunyeoul Lee <eelee.dev@gmail.com>
2025-04-16 00:09:01 +0900

    Update 2주차_1269

--- Solved/이은여울/2주차/2주차-1269.py
+++ Solved/이은여울/2주차/2주차-1269.py
@@ -2,5 +2,6 @@ a_n, b_n = map(int, input().split())
 A = set(map(int, input().split()))
 B = set(map(int, input().split()))
 result = (A-B)|(B-A) # union(|) 합집합 연산자
+# 참고 - set ^ set: 대칭차집합합
 
 print(len(result))
